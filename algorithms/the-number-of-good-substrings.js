61f997a1f9130a843f3f849219dcb0364f3b2b0d
yuxuemei <yuxuemei@iqianjin.com>
2019-09-20 19:04:02 +0800

    The Number Of Good Substrings

---
+++ algorithms/the-number-of-good-substrings.js
@@ -0,0 +1,19 @@
+// https://codeforces.com/problemset/problem/1217/C
+let queryNum;
+const readline = require('readline');
+const process = require('process');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+rl.on('line', (line) => {
+  let cur = line.trim();
+  if (!queryNum) {
+    queryNum = Number(cur);
+  } else {
+    goodCount(cur)
+  }
+});
+
+function goodCount(str) {
+}
