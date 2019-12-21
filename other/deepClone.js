c1d8fbdc375817349aa57aec8ee1ba1c83326799
may <mayxiaoyu1223@gmail.comgit config --global user.name mayxiaoyu>
2019-12-21 19:13:28 +0800

    事件代理

--- other/deepClone.js
+++ other/deepClone.js
@@ -1,6 +1,8 @@
 
 // JSON.stringigy  JSON.parse 在严格的JSON格式下是可以的
 // 深拷贝的核心思想是递归
+// https://github.com/jquery/jquery/blob/1472290917f17af05e98007136096784f9051fab/src/core.js#L121
+// https://segmentfault.com/a/1190000016672263
 function deepClone (obj) {
     if (typeof obj !== 'object' || obj === null) {
         return obj
